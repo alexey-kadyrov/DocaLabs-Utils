b39acf2f5a52df26a0912bfae26e6d023cb5757e
Alexey Kadyrov <alexeikadyrov@gmail.com>
2013-02-08 20:23:24 +0000

    add concurrent tests

--- tests/DocaLabs.Http.Client.Tests/ResponseDeserialization/ResponseParserTests.cs
+++ tests/DocaLabs.Http.Client.Tests/ResponseDeserialization/ResponseParserTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 using DocaLabs.Http.Client.ResponseDeserialization;
 using DocaLabs.Http.Client.Tests._Utils;
 using Machine.Specifications;
@@ -127,6 +130,255 @@ namespace DocaLabs.Http.Client.Tests.ResponseDeserialization
             public override object Deserialize(HttpResponse response, Type resultType)
             {
                 return new ThirdResult();
+            }
+        }
+    }
+
+    [Subject(typeof(ResponseParser))]
+    class when_parsing_response_parsers_are_choosen_in_order_that_they_were_set : response_deserialization_test_context
+    {
+        static IList<IResponseDeserializationProvider> original_providers;
+        static int order;
+        static Result result;
+
+        Cleanup after_each =
+            () => ResponseParser.Providers = original_providers;
+
+        Establish context = () =>
+        {
+            order = 1;
+
+            Setup("application/json", new MemoryStream());
+
+            original_providers = ResponseParser.Providers;
+
+            ResponseParser.Providers = new List<IResponseDeserializationProvider>
+            {
+                new FirstDeserializer(),
+                new SecondDeserializer(),
+                new ThirdDeserializer()
+            };
+        };
+
+        Because of =
+            () => result = (Result) ResponseParser.Parse(mock_request.Object, typeof (Result));
+
+        It should_deserialize =
+            () => result.Value.ShouldEqual(3);
+
+
+        class Result
+        {
+            public int Value { get; set; }
+        }
+
+        class FirstDeserializer : IResponseDeserializationProvider
+        {
+            public object Deserialize(HttpResponse response, Type resultType)
+            {
+                return new Result { Value = 1 };
+            }
+
+            public bool CanDeserialize(HttpResponse response, Type resultType)
+            {
+                if(order != 1)
+                    throw new SpecificationException("Should have been called first.");
+
+                order++;
+
+                return false;
+            }
+        }
+
+        class SecondDeserializer : IResponseDeserializationProvider
+        {
+            public object Deserialize(HttpResponse response, Type resultType)
+            {
+                return new Result { Value = 2 };
+            }
+
+            public bool CanDeserialize(HttpResponse response, Type resultType)
+            {
+                if (order != 2)
+                    throw new SpecificationException("Should have been called second.");
+
+                order++;
+
+                return false;
+            }
+        }
+
+        class ThirdDeserializer : IResponseDeserializationProvider
+        {
+            public object Deserialize(HttpResponse response, Type resultType)
+            {
+                return new Result { Value = 3 };
+            }
+
+            public bool CanDeserialize(HttpResponse response, Type resultType)
+            {
+                if (order != 3)
+                    throw new SpecificationException("Should have been called third.");
+
+                order++;
+
+                return resultType == typeof(Result);
+            }
+        }
+    }
+
+    [Subject(typeof(ResponseParser))]
+    class when_response_parser_is_used_concurrently 
+    {
+        static IList<IResponseDeserializationProvider> original_providers;
+        static WebRequest request;
+        static int count;
+        static object locker;
+        static Random random;
+
+        Cleanup after_each =
+            () => ResponseParser.Providers = original_providers;
+
+        Establish context = () =>
+        {
+            original_providers = ResponseParser.Providers;
+
+            ResponseParser.Providers = new List<IResponseDeserializationProvider>
+            {
+                new FirstDeserializer(),
+                new SecondDeserializer(),
+                new ThirdDeserializer()
+            };
+
+            count = 0;
+            locker = new object();
+            random = new Random();
+
+            request = new TestRequest();
+        };
+
+        Because of = () => Parallel.For(0, 1000000, i =>
+        {
+            int ii;
+
+            lock (locker)
+            {
+                ii = random.Next(7);
+            }
+
+            switch (ii)
+            {
+                case 0:
+                    ResponseParser.Providers = new List<IResponseDeserializationProvider>
+                    {
+                        new FirstDeserializer(),
+                        new SecondDeserializer(),
+                        new ThirdDeserializer()
+                    };
+                    break;
+                case 1:
+                    ResponseParser.Providers.Count.ShouldEqual(3);
+                    break;
+                case 2:
+                    ResponseParser.Parse(request, typeof(FirstResult)).ShouldBeOfType<FirstResult>();
+                    break;
+                case 3:
+                    ResponseParser.Parse(request, typeof(SecondResult)).ShouldBeOfType<SecondResult>();
+                    break;
+                case 4:
+                    ResponseParser.Parse(request, typeof(ThirdResult)).ShouldBeOfType<ThirdResult>();
+                    break;
+                case 5:
+                    ResponseParser.Parse(request, typeof(VoidType)).ShouldNotBeTheSameAs(VoidType.Value);
+                    break;
+                case 6:
+                    Catch.Exception(() => ResponseParser.Parse(request, typeof(ForthResult))).ShouldBeOfType<UnrecoverableHttpClientException>();
+                    break;
+            }
+
+            Interlocked.Increment(ref count);
+        });
+
+        It should_execute_all_operations =
+            () => count.ShouldEqual(1000000);
+
+        class FirstResult
+        {
+        }
+
+        class SecondResult
+        {
+        }
+
+        [AttributeForThirdDeserializer]
+        class ThirdResult
+        {
+        }
+
+        class ForthResult
+        {
+        }
+
+        class FirstDeserializer : IResponseDeserializationProvider
+        {
+            public object Deserialize(HttpResponse response, Type resultType)
+            {
+                return new FirstResult();
+            }
+
+            public bool CanDeserialize(HttpResponse response, Type resultType)
+            {
+                return resultType == typeof(FirstResult);
+            }
+        }
+
+        class SecondDeserializer : IResponseDeserializationProvider
+        {
+            public object Deserialize(HttpResponse response, Type resultType)
+            {
+                return new SecondResult();
+            }
+
+            public bool CanDeserialize(HttpResponse response, Type resultType)
+            {
+                return resultType == typeof(SecondResult);
+            }
+        }
+
+        class ThirdDeserializer : IResponseDeserializationProvider
+        {
+            public object Deserialize(HttpResponse response, Type resultType)
+            {
+                return null;
+            }
+
+            public bool CanDeserialize(HttpResponse response, Type resultType)
+            {
+                return resultType == typeof(ThirdResult);
+            }
+        }
+
+        class AttributeForThirdDeserializerAttribute : ResponseDeserializationAttribute
+        {
+            public override object Deserialize(HttpResponse response, Type resultType)
+            {
+                return new ThirdResult();
+            }
+        }
+
+        class TestResponse : WebResponse
+        {
+            public override Stream GetResponseStream()
+            {
+                return new MemoryStream();
+            }  
+        }
+
+        class TestRequest : WebRequest
+        {
+            public override WebResponse GetResponse()
+            {
+                return new TestResponse();
             }
         }
     }
